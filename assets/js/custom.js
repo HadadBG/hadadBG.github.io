683465b014dba7c42c1fd01bdadeb7605f7e74f2
Hadad Bautista <hadad.bautista@gmail.com>
2026-04-28 17:18:42 -0600

    Added responsive design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,55 @@
+
 document.addEventListener("DOMContentLoaded", function() {
 
 document.getElementById("sendEmail").addEventListener("click", function(event) {
-   let nombre = document.getElementById("name").value;
-emailjs.init("kP12sgvGUS74sBu7R");
-    emailjs.send("service_qs1xx1a","template_toxexqp",{
-title: "Nuevo mensaje de contacto "+nombre,
-name: nombre,
-time: new Intl.DateTimeFormat('es-MX', {
-  timeZone: 'America/Mexico_City',
-  dateStyle: 'medium',
-  timeStyle: 'short'
-}).format(new Date()),
-message: document.getElementById("message").value,
-contact: document.getElementById("email").value,
-}).then(() => {
-      
-      alert('Correo enviado');
-    }, (err) => {
-    
-      console.log(JSON.stringify(err));
+    document.getElementById("fecha_envio").value =
+    new Date().toLocaleString("es-MX", {
+      timeZone: "America/Mexico_City"
     });
-
-
-});
+    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+    document.getElementById("contactForm").submit(); // Envía el formulario
+    alert("Enviando Correo... ¡Gracias por contactarme!");
 
 })
+});
+
+
+const isMobile = window.innerWidth < 768;
+
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: isMobile ? 20 : 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.4 },
+    size: {
+      value: isMobile ? 3 : 5,
+      random: true
+    },
+    line_linked: {
+      enable: true, 
+      distance: 200,
+      color: "#ffffff",
+      opacity: 0.5,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: isMobile ? 1 : 3, 
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: false },
+      onclick: { enable: false }
+    }
+  },
+  retina_detect: !isMobile 
+});
